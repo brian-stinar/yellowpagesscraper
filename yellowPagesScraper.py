ef824df72fc2e6e16a5129ab1d4d23a267f7c1f3
Brian Stinar <brian@novetum.us>
2013-09-21 11:27:16 -0600

    Another not working attempt, using urllib2

--- yellowPagesScraper.py
+++ yellowPagesScraper.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python
 # -*- coding: latin-1 -*-
 
-#import dryscrape - scraping library not working so well - it looks like YellowPages is doing some tricky stuff for Webkit based browsers
+# import dryscrape - scraping library not working so well - it looks like YellowPages is doing some tricky stuff for Webkit based browsers
+# import urllib2 -- same nastiness as above.
 import time
 import random
 import os
@@ -12,10 +13,13 @@ class YellowPagesScraper():
     def __init__(self):
         self.pagePosition = 0 # Where are we in our page list
         self.shortSleepMaxSeconds = 5
+        self.url = "http://www.yellowpages.com/"
         
         now = datetime.datetime.now() # Get the current datetime
         self.nowString =  str(now.year)  + '.' + str(now.month) + '.' + str(now.day) + '.' + str(now.hour) 
-        
+        if not os.path.isdir(self.nowString):
+            os.mkdir(self.nowString) # make a directory with the data, if it doesn't exist 
+        os.chdir(self.nowString) # change into this directory
     
     
     def shortRandomSleep(self):
@@ -23,9 +27,19 @@ class YellowPagesScraper():
     
     
     
-    def spider(self, keyWord, location):
+    def spider(self, keyWord, zipcode):
         # set up a web scraping session
+        keyWordDashes = keyWord.replace(' ', '-')
+        keyWordPlus = keyWord.replace(' ', '+')
         
+        url = self.url + zipcode + '/' + keyWordDashes + '?q=' + keyWordPlus
+        response = urllib2.urlopen(url)
+        html = response.read()
+        
+        print html
+
+        #http://www.yellowpages.com/87106/sports-gym?q=Sports+Gym
+
         '''
         sess = dryscrape.Session(base_url = 'http://www.yellowpages.com')
     
@@ -50,4 +64,4 @@ class YellowPagesScraper():
 
 if __name__ == "__main__":
     scraper = YellowPagesScraper()
-    scraper.spider("Gym", "Albuquerque, New Mexico")
+    scraper.spider("Gym", "87106")
