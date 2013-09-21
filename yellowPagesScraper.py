6bcd732d45f25e2ab45835e00293b7469191828b
Brian Stinar <brian@novetum.us>
2013-09-21 12:19:25 -0600

    Basic page scraping looks good. Now I'm starting on parsing.

--- yellowPagesScraper.py
+++ yellowPagesScraper.py
@@ -21,16 +21,16 @@ class YellowPagesScraper():
             os.mkdir(self.nowString) # make a directory with the data, if it doesn't exist 
         os.chdir(self.nowString) # change into this directory
     
-    
+    # TODO - shared code between these two methods. Abstract out the commonalities.
     def shortRandomSleep(self):
         sleepTime = random.random() * self.shortSleepMaxSeconds
-        print("sleeping for " + sleepTime)
+        print("sleeping for " + str(sleepTime) + ' seconds')
         time.sleep(sleepTime)
     
     
     def mediumRandomSleep(self):
         sleepTime = random.random() * self.mediumSleepMaxSeconds
-        print("sleeping for " + sleepTime)
+        print("sleeping for " + str(sleepTime) + ' seconds')
         time.sleep(sleepTime)
     
     
@@ -48,7 +48,11 @@ class YellowPagesScraper():
         currentPage = 2
         
         for i in range(numberOfPagesToGrab):
+            # The first URL is different
+            if i == 1:
+                continue
             self.mediumRandomSleep()
+
             #http://www.yellowpages.com/87106/sports-gym?o=0&page=2&q=Sports+Gym
             url = self.url + zipcode + '/' + keyWordDashes + '?o=0&page=' + str(currentPage) + '?q=' + keyWordPlus
             print keyWordDashes
@@ -56,7 +60,15 @@ class YellowPagesScraper():
             command = 'wget --output-document ' + str(currentPage) + '.html ' + url
             print command
             os.system(command)
+            currentPage = currentPage + 1
+    
+    def parsePages(self):
+        # Get a page listing of all the pages in the current directory
+        fileList = os.listdir(".")
+        for fileName in fileList:
+            continue
         
 if __name__ == "__main__":
     scraper = YellowPagesScraper()
-    scraper.spider("Gym", "87106", 2)
+    # scraper.spider("Gym", "87106", 5)
+    scraper.parsePages()
