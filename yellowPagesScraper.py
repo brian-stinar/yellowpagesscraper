14de45c2e5dc4751665a712fed601a1b31b32e72
Brian Stinar <brian@novetum.us>
2013-09-21 11:34:03 -0600

    Wget looks like it's giving me legit results. I'll try using BeautifulSoup to grab the URL before I go down the wget path.

--- yellowPagesScraper.py
+++ yellowPagesScraper.py
@@ -2,7 +2,7 @@
 # -*- coding: latin-1 -*-
 
 # import dryscrape - scraping library not working so well - it looks like YellowPages is doing some tricky stuff for Webkit based browsers
-import urllib2 #-- same nastiness as above.
+#import urllib2 #-- same nastiness as above.
 import time
 import random
 import os
@@ -27,41 +27,14 @@ class YellowPagesScraper():
     
     
     
-    def spider(self, keyWord, zipcode):
+    def spider(self, keyWord, zipcode, numberOfPagesToGrab=1):
         # set up a web scraping session
         keyWordDashes = keyWord.replace(' ', '-')
         keyWordPlus = keyWord.replace(' ', '+')
         
         url = self.url + zipcode + '/' + keyWordDashes + '?q=' + keyWordPlus
-        response = urllib2.urlopen(url)
-        html = response.read()
-        
-        print html
-
-        #http://www.yellowpages.com/87106/sports-gym?q=Sports+Gym
-
-        '''
-        sess = dryscrape.Session(base_url = 'http://www.yellowpages.com')
-    
-        # we don't need images
-        sess.set_attribute('auto_load_images', False)
-    
-        # visit homepage and submit a login
-        sess.visit('/')
-        searchTerms = sess.at_xpath('//*[@name="search_terms"]')
-        searchTerms.set(keyWord)
-        searchLocation = sess.at_xpath('//*[@name="geo_location_terms"]')
-        searchLocation.set(location)
-        
-        self.shortRandomSleep() # Otherwise, we get a Operation canceled (error code 5) - likely because yellow pages doesn't want us hitting them as fast as possible
-        
-        searchTerms.form().submit()
-        self.shortRandomSleep()
-        sessionString = sess.body()
-    
-        print sessionString
-        '''
-
+        os.system('wget ' + url)
+ 
 if __name__ == "__main__":
     scraper = YellowPagesScraper()
     scraper.spider("Gym", "87106")
